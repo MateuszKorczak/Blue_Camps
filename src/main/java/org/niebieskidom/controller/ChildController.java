741011bb0506fa9e7056bfe77273efa7b4ecb718
Mateusz <mateusz91korczak@gmail.com>
2020-11-21 17:53:15 +0100

    creating AppConfig and AppInitializer, and add small fixes to entity, and persistence.xml also.

---
+++ src/main/java/org/niebieskidom/controller/ChildController.java
@@ -0,0 +1,9 @@
+package org.niebieskidom.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/children")
+public class ChildController {
+}
